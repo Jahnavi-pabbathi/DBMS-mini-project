57012a5232e1996cdf9599c5a26d2aec3686d166
Jahnavi pabbathi <85212906+Jahnavi-pabbathi@users.noreply.github.com>
2022-05-24 17:12:00 +0530

    Add files via upload

---
+++ includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost:3306";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName );
